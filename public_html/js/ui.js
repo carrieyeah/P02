bf3648e0ba68092e9c14efe3d5f5365ebb3ed8cd
carrieyeah <ziye.carrie@gmail.com>
2015-03-01 23:32:17 -0800

    update payment and booking css and html

--- public_html/js/ui.js
+++ public_html/js/ui.js
@@ -18,3 +18,28 @@ $('.booking_button').click(function(){
 
 
 });
+
+
+jQuery(function($){
+  $('.cc-number').payment('formatCardNumber');
+  $('.cc-exp').payment('formatCardExpiry');
+  $('.cc-cvc').payment('formatCardCVC');
+
+  $('form').submit(function(e){
+    e.preventDefault();
+    $('input').removeClass('invalid');
+    $('.validation').removeClass('passed failed');
+
+    var cardType = $.payment.cardType($('.cc-number').val());
+
+    $('.cc-number').toggleClass('invalid', !$.payment.validateCardNumber($('.cc-number').val()));
+    $('.cc-exp').toggleClass('invalid', !$.payment.validateCardExpiry($('.cc-exp').payment('cardExpiryVal')));
+    $('.cc-cvc').toggleClass('invalid', !$.payment.validateCardCVC($('.cc-cvc').val(), cardType));
+
+    if ( $('input.invalid').length ) {
+      $('.validation').addClass('failed');
+    } else {
+      $('.validation').addClass('passed');
+    }
+  });
+});
